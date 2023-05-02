9107cd33ed2fb17ad1675b43ae6c2c0bc2607eec
kacpermilan <kacpermmilan@gmail.com>
2023-05-02 19:42:49 +0200

    [feature] Implement the login form

--- FatigueTester/urls.py
+++ FatigueTester/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main_menu, name='main_menu'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     path('start_tests/', views.start_tests, name='start_tests'),
     path('database/', views.database, name='database'),
-    path('login/', views.login, name='login'),
 ]
--- FatigueTester/views.py
+++ FatigueTester/views.py
@@ -1,25 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def main_menu(request):
+    if request.method == 'POST':
+        username = request.POST['email_field']
+        password = request.POST['password_field']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('main_menu')
+
+        else:
+            messages.success(request, "There was an error logging in, please try again")
+            return redirect('login')
+
     return render(request, 'main_menu.html')
 
 
 def login_user(request):
-    pass
+    return render(request, 'login.html')
 
 
 def logout_user(request):
-    pass
-
+    logout(request)
+    messages.success(request, "You have been log out")
+    return redirect('main_menu')
 
 def start_tests(request):
     return render(request, 'tests_web.html')
 
 
 def database(request):
-    pass
-
-
-def login(request):
     pass
