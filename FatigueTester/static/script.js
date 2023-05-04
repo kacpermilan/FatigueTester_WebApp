de5e95d4309e0e1ab4e4fcc5b1df7115dd13b5bb
Atusia <adriannakrol@student.agh.edu.pl>
2023-05-04 19:28:28 +0200

    Zmienia się tez tekst

--- FatigueTester/static/script.js
+++ FatigueTester/static/script.js
@@ -1,20 +1,31 @@
- // Function to change the color of the rectangle
+ //Defining the text in rectangle
+var colors = ["red", "green", "blue", "yellow", "black"];
+
+// Function to change the color of the rectangle and it's text
     function changeColor() {
-    // Do I even use this function?
-    console.log("changeColor() called");
-    // Get a random number between 1 and the number of buttons
-      var randomNumber = Math.floor(Math.random() * 5) + 1;
+
+      // Do I even use this function?
+        console.log("changeColor() called");
+
+      // Get a random number between 1 and 5
+        var randomNumber = Math.floor(Math.random() * 5) + 1;
+        var randomNumbertext = Math.floor(Math.random() * 5);
 
       // Get the color of the corresponding button
-      var buttonColor = window.getComputedStyle(document.querySelector(".button" + randomNumber)).getPropertyValue("background-color");
+        var buttonColor = window.getComputedStyle(document.querySelector(".button" + randomNumber)).getPropertyValue("background-color");
 
       // Set the color of the rectangle to the button color
-      document.getElementById("rectangle").style.backgroundColor = buttonColor;
+        document.getElementById("rectangle").style.backgroundColor = buttonColor;
+
+      //Set text in rectangle to color
+        rectangle.textContent = colors[randomNumbertext];
     }
 
-    // Start the timer
-    var startTime = new Date().getTime();
-    var timerId = setInterval(function() {
+// Start the timer
+var startTime = new Date().getTime();
+
+var timerId = setInterval(function() {
+
         // Calculate the elapsed time
         var currentTime = new Date().getTime();
         var elapsedTime = currentTime - startTime;
@@ -27,8 +38,9 @@
         timeElapsedContainer.textContent = 'Time elapsed: ' + seconds + ' seconds';
     }, 1000);
 
-    // Function to start the timer
-    function startTimer() {
-        // Reset the start time
-        startTime = new Date().getTime();
-    }
+
+// Function to start the timer
+function startTimer() {
+    // Reset the start time
+    startTime = new Date().getTime();
+}
